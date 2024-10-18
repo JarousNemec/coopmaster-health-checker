289f492ae9bc314c0f2b447200f98b4ad80fdd65
JarousNemec <jarous.nemec@gmail.com>
2024-10-19 01:56:09 +0200

    unavailable service fix

--- app/health_checker.py
+++ app/health_checker.py
@@ -6,6 +6,11 @@ from app import configuration
 def check():
     containers_responses = []
     for port in range(configuration.start_port, configuration.end_port):
-        response = requests.get(f'http://{configuration.docker_host}:{port}')
-        containers_responses.append("Service at port:"+str(port)+" - Status code: " + str(response.status_code)+" - "+response.text)
+        try:
+            response = requests.get(f'http://{configuration.docker_host}:{port}')
+            containers_responses.append("Service at port:"+str(port)+" - Status code: " + str(response.status_code)+" - "+response.text)
+        except:
+            containers_responses.append(
+                "Service at port:" + str(port) + " - Service unavailable")
+
     return containers_responses
